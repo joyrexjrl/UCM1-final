3d473d190656bb52fb73896bac10775b5ae5627f
joyrexjrl <ricklozano@gmail.com>
2024-06-12 13:44:42 -0700

    removed dark mode button
    removed the ability to change between light and dark modes, keeping things simple

--- scripts/darkModeScript.js
+++
@@ -1,9 +0,0 @@
-const toggleButton = document.getElementById(`darkModeButton`);
-const htmlElement = document.documentElement;
-
-function toggleMode(){
-    if(htmlElement.classList.contains(`dark-mode`)) htmlElement.classList.remove(`dark-mode`);
-    else htmlElement.classList.add(`dark-mode`);    
-}
-
-toggleButton.addEventListener('click', toggleMode);
